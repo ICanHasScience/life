d916029c3379c8f34f66785c6f129f63679c57b6
James Helton <trealtrix@gmail.com>
2018-03-16 20:40:55 -0400

    Add grid

--- js/drawGrid.js
+++ js/drawGrid.js
@@ -15,7 +15,7 @@ function drawGrid(ctx, minor, major, stroke, fill) {
 		ctx.moveTo(x,0);
 		ctx.lineTo(x, ctx.canvas.height);
 		ctx.lineWidth = x % major == 0 ? 0.5 : 0.25;
-		if(x%major == 0) {ctx.fillText(x, x, 10);}
+		// if(x%major == 0) {ctx.fillText(x, x, 10);}
 		ctx.stroke();
 	}
 	for (let y = 0; y < ctx.canvas.height; y+=minor)
@@ -24,7 +24,7 @@ function drawGrid(ctx, minor, major, stroke, fill) {
 		ctx.moveTo(0, y);
 		ctx.lineTo(ctx.canvas.width, y);
 		ctx.lineWidth = y % major == 0 ? 0.5 : 0.25;
-		if(y%major == 0) {ctx.fillText(y, 0, y + 10);}
+		// if(y%major == 0) {ctx.fillText(y, 0, y + 10);}
 		ctx.stroke();
 	}
 
